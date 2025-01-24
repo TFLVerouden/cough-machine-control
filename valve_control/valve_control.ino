07a34f3458ff4071c94c626af6a9feafd9e0db61
Tommie Verouden <t.f.l.verouden@utwente.nl>
2025-01-24 11:48:51 +0100

    Updated comments

--- valve_control/valve_control.ino
+++ valve_control/valve_control.ino
@@ -5,57 +5,91 @@ bool timeSet = false;  // Flag to check if time has been set
 bool valveOpen = false;  // Flag to check if the valve is open
 
 void setup() {
-  pinMode(ledPin, OUTPUT);  // Set the LED pin as an output
-  pinMode(powerPin, OUTPUT);  // Set the power pin as an output
-  digitalWrite(ledPin, LOW);  // Ensure the LED is off initially
-  digitalWrite(powerPin, LOW);  // Ensure the power pin is off initially
-  Serial.begin(9600);  // Start the serial communication
-  Serial.println("Arduino is ready");
+  // Set the outputs
+  pinMode(ledPin, OUTPUT);
+  pinMode(powerPin, OUTPUT);
+
+  // Ensure initial off states
+  digitalWrite(ledPin, LOW);
+  digitalWrite(powerPin, LOW);
+
+  // Start the serial communication
+  Serial.begin(9600); 
+  Serial.println("ARDUINO ready");
 }
 
 void loop() {
+
+  // If serial communication has started...
   if (Serial.available() > 0) {
+
+    // ...await commands
     String command = Serial.readStringUntil('\n');
-    Serial.print("Received command: ");
+    Serial.print("ARDUINO received command: ");
     Serial.println(command);
+
     if (command.startsWith("TIME")) {
+
+      // Get duration from TIME command
       duration = command.substring(5).toInt();
-      Serial.print("Setting time to: ");
+      Serial.print("ARDUINO setting opening duration to: ");
       Serial.print(duration);
       Serial.println(" ms");
       timeSet = true;
+
     } else if (command == "OPEN") {
+
+      // Accept the OPEN command if the duration has been set
       if (timeSet) {
-        Serial.println("Valve is OPEN");
-        digitalWrite(ledPin, HIGH);  // Turn the LED on
-        digitalWrite(powerPin, HIGH);  // Turn the power pin on
+
+        // Turn on pins
+        Serial.println("ARDUINO opened the valve");
+        digitalWrite(ledPin, HIGH);
+        digitalWrite(powerPin, HIGH);
         valveOpen = true;
+
+        // Start counting
         unsigned long startTime = millis();
         while (millis() - startTime < duration) {
+
+          // Listen for interruptions
           if (Serial.available() > 0) {
             String interruptCommand = Serial.readStringUntil('\n');
-            if (interruptCommand == "CLOSE" || interruptCommand == "RESET") {
-              Serial.println("Manually closing the valve");
+            if (interruptCommand == "CLOSE") {
+
+              // Closing the valve
               digitalWrite(ledPin, LOW);  // Turn the LED off
               digitalWrite(powerPin, LOW);  // Turn the power pin off
-              Serial.println("Valve is CLOSED");
+              Serial.println("ARDUINO closed the valve");
               valveOpen = false;
-              if (interruptCommand == "RESET") {
-                Serial.println("Closing valve and resetting time");
-                timeSet = false;
-              }
-              break;
+
+            } else if (interruptCommand == "RESET") {
+
+              // Closing the valve and resetting time
+              digitalWrite(ledPin, LOW);  // Turn the LED off
+              digitalWrite(powerPin, LOW);  // Turn the power pin off
+              Serial.println("ARDUINO closed the valve and has reset duration");
+              valveOpen = false;
+              timeSet = false;
             }
+
+            // Interrupt counting
+            break;
           }
         }
+
+        // After counting
         if (valveOpen) {
           digitalWrite(ledPin, LOW);  // Turn the LED off
           digitalWrite(powerPin, LOW);  // Turn the power pin off
-          Serial.println("Valve is CLOSED");
+          Serial.println("ARDUINO closed the valve");
           valveOpen = false;
         }
+
+      // Error if duration has not been set beforehand
       } else {
         Serial.println("Error: TIME not set");
+
         // Blink the LED to indicate an error
         for (int i = 0; i < 5; i++) {
           digitalWrite(ledPin, HIGH);
@@ -64,6 +98,8 @@ void loop() {
           delay(100);
         }
       }
+
+    
     } else if (command == "CLOSE") {
       if (valveOpen) {
         Serial.println("Manually closing the valve");
