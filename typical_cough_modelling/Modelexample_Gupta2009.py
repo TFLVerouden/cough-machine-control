7ed84390a52b3e4a8e283290b0f75c6ef2aec230
abes002 <abesikkema@gmail.com>
2025-01-31 14:34:49 +0100

    Added colorblindfriendly code, and way to store data easily. Plus a .png of the Gupta model

--- typical_cough_modelling/Modelexample_Gupta2009.py
+++ typical_cough_modelling/Modelexample_Gupta2009.py
@@ -1,7 +1,24 @@
 import numpy as np
 import scipy as sc
 import matplotlib.pyplot as plt
+import os
 import Gupta2009 as Gupta
+#initalizing
+plt.style.use('tableau-colorblind10')
+font = {'size'   : 14}
+#linestyles: supported values are '-', '--', '-.', ':', 'None', ' ', '', 'solid', 'dashed', 'dashdot', 'dotted'
+plt.rc('font', **font)
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+print(f"Script is located in: {script_dir}")
+
+# Define the path for the new "results" folder
+path = os.path.join(script_dir, "results")
+
+# Create the "results" folder if it doesn't exist
+os.makedirs(path, exist_ok=True)
+
 
 
 #testing
@@ -46,13 +63,13 @@ PVT_E, CPFR_E, CEV_E = Gupta.estimator("Male",70, 1.89)
 cough_E = Gupta.M_model(Tau,PVT_E,CPFR_E,CEV_E)
 
 plt.figure()
-plt.plot(Tau,cough_A,label= "Person A, lower bounds male",c='b')
-plt.plot(Tau,cough_B,label= "Person B, higher bounds male",c='r')
-plt.plot(Tau,cough_C,label= "Person C, lower bounds female",c='g')
-plt.plot(Tau,cough_D,label= "Person D, higher bounds female",c='k')
-plt.plot(Tau,cough_E,label= "Me",c='orange')
+plt.plot(Tau,cough_A,label= "Person A, lower bounds male", linestyle= "-")
+plt.plot(Tau,cough_B,label= "Person B, higher bounds male", linestyle= "--")
+plt.plot(Tau,cough_C,label= "Person C, lower bounds female",linestyle= "-.")
+plt.plot(Tau,cough_D,label= "Person D, higher bounds female",linestyle= ":")
+plt.plot(Tau,cough_E,label= "Me",c='orange', linestyle= ":", linewidth= 5)
 plt.xlabel(r"$\tau$")
 plt.ylabel("M")
 plt.legend()
-plt.show()
+plt.savefig(path+ "\Gupta2009_5modeledcases.png")
 
