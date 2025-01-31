92b5c80562f40c2b1a7fb17a93ffb7c8621986df
abes002 <abesikkema@gmail.com>
2025-01-31 12:01:15 +0100

    Modelled cough Gupta 2009
    First setup of Gupta 2009 modelled cough

---
+++ typical_cough_modelling/Modelexample_Gupta2009.py
@@ -0,0 +1,51 @@
+import numpy as np
+import scipy as sc
+import matplotlib.pyplot as plt
+import Gupta2009 as Gupta
+
+#testing
+
+Tau = np.linspace(0,10,101)
+
+#person A lowest values of Male from Gupta et al 2009
+
+CPFR_A = 3 #L/s
+CEV_A = 0.400 #L
+PVT_A = 57E-3 #s
+
+cough_A = Gupta.M_model(Tau,PVT_A,CPFR_A,CEV_A)
+
+#person B highest values of Male from Gupta et al 2009
+
+CPFR_B = 8.5 #L/s
+CEV_B = 1.6 #L
+PVT_B = 96E-3 #s
+
+cough_B = Gupta.M_model(Tau,PVT_B,CPFR_B,CEV_B)
+
+#person C lowest values of feale from Gupta et al 2009
+
+CPFR_C= 1.6 #L/s
+CEV_C = 0.25 #L
+PVT_C = 57E-3 #s
+
+cough_C = Gupta.M_model(Tau,PVT_C,CPFR_C,CEV_C)
+
+#person D highest values of Male from Gupta et al 2009
+
+CPFR_D = 6 #L/s
+CEV_D = 1.25 #L
+PVT_D = 110E-3 #s
+
+cough_D = Gupta.M_model(Tau,PVT_D,CPFR_D,CEV_D)
+
+plt.figure()
+plt.plot(Tau,cough_A,label= "Person A, lower bounds male",c='b')
+plt.plot(Tau,cough_B,label= "Person B, higher bounds male",c='r')
+plt.plot(Tau,cough_C,label= "Person C, lower bounds female",c='g')
+plt.plot(Tau,cough_D,label= "Person D, higher bounds female",c='k')
+plt.xlabel(r"$\tau$")
+plt.ylabel("M")
+plt.legend()
+plt.show()
+
