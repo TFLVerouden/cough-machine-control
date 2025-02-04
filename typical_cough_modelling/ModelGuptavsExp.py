0a4d860c4a0d4a3e7d92259add0427211968119d
abes002 <abesikkema@gmail.com>
2025-02-04 11:51:25 +0100

    Added code (ModelGuptavsExp.py) to process first experimental data and compare it to model.

---
+++ typical_cough_modelling/ModelGuptavsExp.py
@@ -0,0 +1,129 @@
+import numpy as np
+import scipy as sc
+import matplotlib.pyplot as plt
+import os
+import Gupta2009 as Gupta
+import re
+import pandas as pd
+
+def sorting_legend(handles,labels,suffix= ""):
+    
+    # Separate numeric and non-numeric labels
+    numeric_labels = []
+    non_numeric_labels = []
+
+    for label, handle in zip(labels, handles):
+        if label.isdigit():  # Check if the label is numeric
+            numeric_labels.append((label, handle))
+        else:
+            non_numeric_labels.append((label, handle))
+
+    # Sort numeric labels
+    sorted_numeric_labels = sorted(numeric_labels, key=lambda x: int(x[0]))
+
+    # Combine sorted numeric labels with non-numeric labels (non-numeric at the end)
+    sorted_handles_labels = sorted_numeric_labels + non_numeric_labels
+
+    # Unzip the sorted labels and handles
+    sorted_labels, sorted_handles = zip(*sorted_handles_labels)
+
+    # Optionally add " ms" to numeric labels
+    sorted_labels = [f"{label} {suffix}" if label.isdigit() else label for label in sorted_labels]
+
+    return sorted_handles,sorted_labels
+#initalizing
+plt.style.use('tableau-colorblind10')
+font = {'size'   : 14}
+#linestyles: supported values are '-', '--', '-.', ':', 'None', ' ', '', 'solid', 'dashed', 'dashdot', 'dotted'
+plt.rc('font', **font)
+colors = ["blue", "red", "green", "purple","black", "Orange"]
+linestyles = ["-", "--", "-.", ":", "-", "--"]
+#model
+
+#person E, me based on Gupta et al
+Tau = np.linspace(0,10,101)
+
+PVT_E, CPFR_E, CEV_E = Gupta.estimator("Male",70, 1.89)
+
+cough_E = Gupta.M_model(Tau,PVT_E,CPFR_E,CEV_E)
+
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+print(f"Script is located in: {script_dir}")
+
+# Define the path for the new "results" folder
+path = os.path.join(script_dir, "results")
+datapath = os.path.join(script_dir, "data_experiments")
+
+csv_files = [f for f in os.listdir(datapath) if f.endswith('.csv')]
+extracted_parts = [re.search(r'[^_]+_[^_]+(?=\.csv)', f).group() for f in csv_files if re.search(r'[^_]+_[^_]+(?=\.csv)', f)]
+
+
+
+# Create the "results" folder if it doesn't exist
+os.makedirs(path, exist_ok=True)
+
+
+#### plotting
+fig, ax1 = plt.subplots(figsize= (8,8))
+opening_durations = pd.Series([],dtype=float)
+ax1.set_xlim(0, 1.1)
+ax1.set_ylim(0, 8)
+
+
+for i,csv_file in enumerate(csv_files):
+    csv_path = os.path.join(datapath,csv_file)
+    metadata = pd.read_csv(csv_path,nrows=5,header=None,index_col=0)
+    df = pd.read_csv(csv_path,skiprows=7,names=["Time", "Pressure", "Flow"],index_col=0,header=None)
+
+    opening_duration = metadata.loc['Opening duration (ms)']
+
+    ###I want to sort them on plotting style
+
+    if opening_durations.empty:
+        label= list(opening_duration.values)[0]
+        opening_durations = pd.concat([opening_durations,opening_duration],ignore_index=True)
+
+    elif  opening_durations.isin(opening_duration).any():
+
+        label= "_nolegend_"
+
+    else:
+        label= list(opening_duration.values)[0]
+        opening_durations = pd.concat([opening_durations,opening_duration],ignore_index=True)
+
+    plot_ind_mask = opening_durations.values == opening_duration.values
+    plot_ind = list(opening_durations[plot_ind_mask].index)[0]
+  
+
+    #Processing the data for future purposes
+
+    dt = np.diff(df.index)
+    mask = df.loc[:,'Flow']>0 #finds the first time the flow rate is above 0
+    t0 = df.index[mask][0]
+    peak_ind = np.argmax(df.loc[:,'Flow'])
+   
+    PVT = df.index[peak_ind] - t0 #Peak velocity time
+    CFPR = df.iloc[peak_ind, 1] #Critical flow pressure rate (L/s)
+    CEV = np.sum(dt * df.iloc[1:,1]) #Cumulative expired volume
+    df = df[mask]
+    t = df.index - t0
+
+    ax1.plot(t, df.loc[:,'Flow'],color= colors[plot_ind],linestyle = linestyles[plot_ind],label=label)
+ax1.plot(Tau*PVT_E,cough_E* CPFR_E,label= "Me", linestyle= ":", linewidth= 5)
+
+ax1.set_xlabel('Time (s)')
+ax1.set_ylabel('Flow rate (L/s)')
+ax1.set_title(f'Comparison of model and experimental based on opening time')
+ax1.grid()
+##sorting the legend
+handles, labels = plt.gca().get_legend_handles_labels()
+sorted_handles,sorted_labels = sorting_legend(handles,labels,suffix= "ms")
+
+# Add the legend outside of the plot area (e.g., upper left, outside)
+plt.legend(sorted_handles, sorted_labels, loc='upper right')
+###
+plt.savefig(path+ "\Modelvsexp_openingtime.png")
+plt.show()
+
