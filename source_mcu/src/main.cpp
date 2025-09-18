881a6b4268ebf281a62d0905050c8c8a61d833e7
Tommie Verouden <t.f.l.verouden@utwente.nl>
2025-09-18 15:25:02 +0200

    Formatting (get clanged!)

--- source_mcu/src/main.cpp
+++ source_mcu/src/main.cpp
@@ -1,33 +1,32 @@
-#include <Arduino.h>
-#include <Adafruit_SHT4x.h>
 #include "MIKROE_4_20mA_RT_Click.h"
+#include <Adafruit_SHT4x.h>
+#include <Arduino.h>
 
 // Connections
-const int PIN_VALVE = 7;         // Pin to control the MOSFET for valve control
-const int PIN_CS_RCLICK = 2;          // Cable select pin for RClick pressure reading
-const int PIN_TRIG = 9;           // Pin for trigger out 
+const int PIN_VALVE = 7;     // Pin to control the MOSFET for valve control
+const int PIN_CS_RCLICK = 2; // Cable select pin for RClick pressure reading
+const int PIN_TRIG = 9;      // Pin for trigger out
 
 // Trigger parameters
 const uint32_t TRIGGER_WIDTH = 10000; // Trigger pulse width [µs]
-uint32_t tick = 0;  // Trigger for the tick interval
+uint32_t tick = 0;                    // Trigger for the tick interval
 bool performingTrigger = false;
 
-// Valve opening logic parameters (these are not static as they are called by functions)
-uint32_t duration = 0;                // Variable to store the duration
-bool valveOpen = false;          // Flag to check if the valve is open
+// Valve opening logic parameters (these are not static as they are called by
+// functions)
+uint32_t duration = 0;  // Variable to store the duration
+bool valveOpen = false; // Flag to check if the valve is open
 
-// Exponential moving average (EMA) parameters & calibration of the R Click readings
+// Exponential moving average (EMA) parameters & calibration of the R Click
+// readings
 const uint32_t EMA_INTERVAL = 500; // Desired oversampling interval [µs]
-const float EMA_LP_FREQ = 200.;      // Low-pass filter cut-off frequency [Hz]
+const float EMA_LP_FREQ = 200.;    // Low-pass filter cut-off frequency [Hz]
 R_Click R_click(PIN_CS_RCLICK, RT_Click_Calibration{3.99, 9.75, 795, 1943});
 
 // Humidity+temperature sensor
-Adafruit_SHT4x sht4;  // Declare the T+RH sensor object
+Adafruit_SHT4x sht4; // Declare the T+RH sensor object
 
-enum State {
-  IDLE,
-  ERROR
-};
+enum State { IDLE, ERROR };
 
 State currentState = IDLE;
 
@@ -40,20 +39,21 @@ void setup() {
   digitalWrite(PIN_LED, LOW);
   digitalWrite(PIN_VALVE, LOW);
   digitalWrite(PIN_TRIG, LOW);
-  
+
   Serial.begin(115200);
   R_click.begin();
 
   // Initialize the SHT4x sensor
   if (!sht4.begin()) {
     Serial.println("Failed to find SHT4x sensor!");
-    while (1) delay(10);  // Halt execution if sensor is not found
+    while (1)
+      delay(10); // Halt execution if sensor is not found
   }
 
   // Set sensor precision and heater mode
   sht4.setPrecision(SHT4X_HIGH_PRECISION);
   sht4.setHeater(SHT4X_NO_HEATER);
-} 
+}
 
 void closeValve() {
   // digitalWrite(PIN_VALVE, LOW);
@@ -64,7 +64,8 @@ void closeValve() {
   Serial.println("!");
 }
 
-// TODO: Print error rather than occupying microcontroller with error state -> state machine can then be removed altogether
+// TODO: Print error rather than occupying microcontroller with error state ->
+// state machine can then be removed altogether
 void blinkError() {
   for (int i = 0; i < 5; i++) {
     digitalWrite(PIN_LED, HIGH);
@@ -77,25 +78,25 @@ void blinkError() {
 void readPressure() {
   // Reads out R-click board and converts to pressure
   Serial.print("P");
-  Serial.print(0.6249*R_click.get_EMA_mA() - 2.4882);
+  Serial.print(0.6249 * R_click.get_EMA_mA() - 2.4882);
   Serial.println();
 }
 
 void readTemperature() {
   // Reads Temperature and RH on command;
   sensors_event_t humidity, temp;
-  
+
   // Read temperature and humidity
   sht4.getEvent(&humidity, &temp);
-  
+
   Serial.print("T");
   Serial.println(temp.temperature);
-  
+
   Serial.print("RH");
   Serial.println(humidity.relative_humidity);
 }
 
- // TODO: Move to loop so variables don't have to be global
+// TODO: Move to loop so variables don't have to be global
 void handleCommand(String command) {
   if (command.startsWith("O")) {
     // Extract duration (us) from command (ms)
@@ -107,7 +108,7 @@ void handleCommand(String command) {
       PORT->Group[g_APinDescription[PIN_VALVE].ulPort].OUTSET.reg =
           ((1 << g_APinDescription[PIN_VALVE].ulPin) |
            (1 << g_APinDescription[PIN_TRIG].ulPin));
-      
+
       digitalWrite(PIN_LED, HIGH);
       valveOpen = true;
       performingTrigger = true;
@@ -135,7 +136,7 @@ void loop() {
   if (performingTrigger && (micros() - tick >= TRIGGER_WIDTH)) {
     // digitalWrite(PIN_TRIG, LOW);
     PORT->Group[g_APinDescription[PIN_TRIG].ulPort].OUTCLR.reg =
-      (1 << g_APinDescription[PIN_TRIG].ulPin);
+        (1 << g_APinDescription[PIN_TRIG].ulPin);
     performingTrigger = false;
   }
 
@@ -155,14 +156,13 @@ void loop() {
   }
 
   switch (currentState) {
-    case IDLE:
-      // Do nothing
-      break;
+  case IDLE:
+    // Do nothing
+    break;
 
-    case ERROR:
-      blinkError();
-      currentState = IDLE;
-      break;
+  case ERROR:
+    blinkError();
+    currentState = IDLE;
+    break;
   }
 }
-
