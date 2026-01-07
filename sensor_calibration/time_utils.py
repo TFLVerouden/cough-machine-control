aa1dab0dd88d1e528d790782911b51dc5d4410cb
Tommie Verouden <t.f.l.verouden@utwente.nl>
2026-01-07 11:57:14 +0100

    Moved calibration script, improved (meta)data file generation and generalised file dialog utilities

--- sensor_calibration/time_utils.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime
-
-
-def timestamp_str() -> str:
-    """Return a standard timestamp string (local time) for filenames."""
-    return datetime.now().strftime("%Y%m%d_%H%M%S")
